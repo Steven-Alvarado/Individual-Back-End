8ac9a586c3dce4d89d4021d6501cff8ef07e8121
Steven-Alvarado <Steven67Alvarado@gmail.com>
2024-09-23 12:18:28 -0400

    Spring Boot setup

---
+++ src/main/java/StevenAlvaradoCS490/Individual_Back_End/IndividualBackEndApplication.java
@@ -0,0 +1,13 @@
+package StevenAlvaradoCS490.Individual_Back_End;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndividualBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndividualBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/StevenAlvaradoCS490/Individual_Back_End/IndividualBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package StevenAlvaradoCS490.Individual_Back_End;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndividualBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
